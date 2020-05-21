3b4546d9e66527f5118c02700cef8596fb56974d
vietbh-d1 <huuvietuet@gmail.com>
2020-05-21 09:30:08 +0700

    init project

---
+++ app/src/main/java/com/example/customvideoframe/CustomCamera2Capture.java
@@ -0,0 +1,50 @@
+package com.example.customvideoframe;
+
+import android.content.Context;
+import android.hardware.camera2.CameraManager;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.twilio.video.Camera2Capturer;
+import com.twilio.video.VideoCapturer;
+import com.twilio.video.VideoFormat;
+import com.twilio.video.VideoFrame;
+
+public class CustomCamera2Capture extends Camera2Capturer {
+
+    public static final String TAG = "CustomCamera2Capture";
+
+    VideoCapturer.Listener myVideoCapturerListener = new VideoCapturer.Listener() {
+        @Override
+        public void onCapturerStarted(boolean success) {
+            Log.d(TAG, "onCapturerStarted: ");
+        }
+
+        @Override
+        public void onFrameCaptured(@NonNull VideoFrame videoFrame) {
+            Log.d(TAG, "onFrameCaptured: ");
+            //todo process videoFrame
+        }
+    };
+
+    /**
+     * Constructs a Camera2Capturer instance.
+     *
+     * <p><b>Note</b>: It is possible to construct multiple instances with different camera IDs, but
+     * there are often device limitations on how many camera2 sessions can be open.
+     *
+     * @param context  application context
+     * @param cameraId unique identifier of the camera device to open that must be specified in
+     *                 {@link CameraManager#getCameraIdList()}.
+     * @param listener listener of camera 2 capturer events
+     */
+    public CustomCamera2Capture(@NonNull Context context, @NonNull String cameraId, @NonNull Listener listener) {
+        super(context, cameraId, listener);
+    }
+
+    @Override
+    public void startCapture(@NonNull VideoFormat captureFormat, @NonNull VideoCapturer.Listener videoCapturerListener) {
+        super.startCapture(captureFormat, myVideoCapturerListener);
+    }
+}
---
+++ app/src/main/java/com/example/customvideoframe/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.customvideoframe
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.twilio.video.CameraCapturer
+import com.twilio.video.LocalAudioTrack
+import com.twilio.video.LocalVideoTrack
+import com.twilio.video.VideoView
+
+class MainActivity : AppCompatActivity() {
+
+    private val CAMERA_MIC_PERMISSION_REQUEST_CODE = 1
+
+    private var localVideoTrack: LocalVideoTrack? = null
+    private var localVideoView: VideoView? = null
+
+    private val cameraCapturerCompat by lazy {
+        CameraCapturerCompat(this, getAvailableCameraSource())
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        localVideoView = findViewById(R.id.videoView)
+
+        requestPermissionForCameraAndMicrophone()
+
+    }
+
+    private fun getAvailableCameraSource(): CameraCapturer.CameraSource {
+        return if (CameraCapturer.isSourceAvailable(CameraCapturer.CameraSource.FRONT_CAMERA))
+            CameraCapturer.CameraSource.FRONT_CAMERA
+        else
+            CameraCapturer.CameraSource.BACK_CAMERA
+    }
+
+    override fun onResume() {
+        super.onResume()
+        localVideoTrack = if (localVideoTrack == null && checkPermissionForCameraAndMicrophone()) {
+            LocalVideoTrack.create(this,
+                    true,
+                    cameraCapturerCompat.videoCapturer)
+        } else {
+            localVideoTrack
+        }
+        localVideoView?.let { localVideoTrack?.addRenderer(it) }
+
+    }
+
+
+    private fun checkPermissionForCameraAndMicrophone(): Boolean {
+        val resultCamera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        val resultMic = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+
+        return resultCamera == PackageManager.PERMISSION_GRANTED &&
+                resultMic == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermissionForCameraAndMicrophone() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA) ||
+                ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.RECORD_AUDIO)) {
+            Toast.makeText(this,
+                    "R.string.permissions_needed",
+                    Toast.LENGTH_LONG).show()
+        } else {
+            ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO),
+                    CAMERA_MIC_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+}

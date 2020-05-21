f9008eb93146feddd303161723dce43947e8d16d
vietbh-d1 <huuvietuet@gmail.com>
2020-05-21 16:30:24 +0700

    change twilio to 2

--- app/src/main/java/com/example/customvideoframe/CameraCapturerCompat.kt
+++ app/src/main/java/com/example/customvideoframe/CameraCapturerCompat.kt
@@ -13,15 +13,16 @@ import androidx.annotation.RequiresApi
 import com.twilio.video.Camera2Capturer
 import com.twilio.video.CameraCapturer
 import com.twilio.video.VideoCapturer
-import tvi.webrtc.Camera2Enumerator
 
 class CameraCapturerCompat(context: Context, cameraSource: CameraCapturer.CameraSource) {
     private val TAG = "CameraCapturerCompat"
 
-    private var camera1Capturer: CameraCapturer? = null
-    private var camera2Capturer: CustomCamera2Capture? = null
+    private var camera1Capturer: CustomCameraCapturer? = null
+
+    //    private var camera2Capturer: CustomCamera2Capture? = null
     private var frontCameraPair: Pair<CameraCapturer.CameraSource, String>? = null
     private var backCameraPair: Pair<CameraCapturer.CameraSource, String>? = null
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private var cameraManager: CameraManager? = null
     private val camera2Listener = object : Camera2Capturer.Listener {
@@ -39,71 +40,58 @@ class CameraCapturerCompat(context: Context, cameraSource: CameraCapturer.Camera
     }
     val cameraSource: CameraCapturer.CameraSource
         get() {
-            if (usingCamera1()) {
-                return camera1Capturer!!.cameraSource
-            } else {
-                return getCameraSource(camera2Capturer!!.cameraId)
-            }
+            return camera1Capturer!!.cameraSource
         }
+
     /*
      * This property is required because this class is not an implementation of VideoCapturer due to
      * a shortcoming in the Video Android SDK.
      */
     val videoCapturer: VideoCapturer
         get() {
-            if (usingCamera1()) {
                 return camera1Capturer!!
-            } else {
-                return camera2Capturer!!
-            }
         }
 
 
     init {
-        if (Camera2Capturer.isSupported(context) && isLollipopApiSupported()) {
-            cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager?;
-            setCameraPairs(context)
-            camera2Capturer = CustomCamera2Capture(context,
-                    getCameraId(cameraSource),
-                    camera2Listener)
-        } else {
-            camera1Capturer = CameraCapturer(context, cameraSource)
-        }
+            camera1Capturer = CustomCameraCapturer(context, cameraSource)
     }
 
     fun switchCamera() {
-        if (usingCamera1()) {
-            camera1Capturer!!.switchCamera()
-        } else {
-            val cameraSource = getCameraSource(camera2Capturer!!
-                    .cameraId)
-
-            if (cameraSource == CameraCapturer.CameraSource.FRONT_CAMERA) {
-                camera2Capturer!!.switchCamera(backCameraPair!!.second)
-            } else {
-                camera2Capturer!!.switchCamera(frontCameraPair!!.second)
-            }
-        }
+//        if (usingCamera1()) {
+//            camera1Capturer!!.switchCamera()
+//        } else {
+//            val cameraSource = getCameraSource(
+//                camera2Capturer!!
+//                    .cameraId
+//            )
+//
+//            if (cameraSource == CameraCapturer.CameraSource.FRONT_CAMERA) {
+//                camera2Capturer!!.switchCamera(backCameraPair!!.second)
+//            } else {
+//                camera2Capturer!!.switchCamera(frontCameraPair!!.second)
+//            }
+//        }
     }
 
     private fun usingCamera1(): Boolean {
         return camera1Capturer != null
     }
 
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    private fun setCameraPairs(context: Context) {
-        val camera2Enumerator = Camera2Enumerator(context)
-        for (cameraId in camera2Enumerator.deviceNames) {
-            if (isCameraIdSupported(cameraId)) {
-                if (camera2Enumerator.isFrontFacing(cameraId)) {
-                    frontCameraPair = Pair(CameraCapturer.CameraSource.FRONT_CAMERA, cameraId)
-                }
-                if (camera2Enumerator.isBackFacing(cameraId)) {
-                    backCameraPair = Pair(CameraCapturer.CameraSource.BACK_CAMERA, cameraId)
-                }
-            }
-        }
-    }
+//    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+//    private fun setCameraPairs(context: Context) {
+//        val camera2Enumerator = Camera2Enumerator(context)
+//        for (cameraId in camera2Enumerator.deviceNames) {
+//            if (isCameraIdSupported(cameraId)) {
+//                if (camera2Enumerator.isFrontFacing(cameraId)) {
+//                    frontCameraPair = Pair(CameraCapturer.CameraSource.FRONT_CAMERA, cameraId)
+//                }
+//                if (camera2Enumerator.isBackFacing(cameraId)) {
+//                    backCameraPair = Pair(CameraCapturer.CameraSource.BACK_CAMERA, cameraId)
+//                }
+//            }
+//        }
+//    }
 
     private fun getCameraId(cameraSource: CameraCapturer.CameraSource): String {
         return if (frontCameraPair!!.first == cameraSource) {
@@ -142,9 +130,11 @@ class CameraCapturerCompat(context: Context, cameraSource: CameraCapturer.Camera
          * that do not support ImageFormat.PRIVATE output formats. A long term fix is currently in development.
          * https://github.com/twilio/video-quickstart-android/issues/431
          */
-        val streamMap = cameraCharacteristics?.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+        val streamMap =
+            cameraCharacteristics?.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            isPrivateImageFormatSupported = streamMap?.isOutputSupportedFor(ImageFormat.PRIVATE) ?: false
+            isPrivateImageFormatSupported =
+                streamMap?.isOutputSupportedFor(ImageFormat.PRIVATE) ?: false
         }
 
         /*
@@ -152,11 +142,14 @@ class CameraCapturerCompat(context: Context, cameraSource: CameraCapturer.Camera
          * SENSOR_INFO_COLOR_FILTER_ARRANGEMENT_MONO or SENSOR_INFO_COLOR_FILTER_ARRANGEMENT_NIR.
          * Visit this link for details on supported values - https://developer.android.com/reference/android/hardware/camera2/CameraCharacteristics#SENSOR_INFO_COLOR_FILTER_ARRANGEMENT
          */
-        val colorFilterArrangement = cameraCharacteristics?.get(CameraCharacteristics.SENSOR_INFO_COLOR_FILTER_ARRANGEMENT) ?: -1
+        val colorFilterArrangement =
+            cameraCharacteristics?.get(CameraCharacteristics.SENSOR_INFO_COLOR_FILTER_ARRANGEMENT)
+                ?: -1
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            isMonoChromeSupported = (colorFilterArrangement == CameraMetadata.SENSOR_INFO_COLOR_FILTER_ARRANGEMENT_MONO
-                    || colorFilterArrangement == CameraMetadata.SENSOR_INFO_COLOR_FILTER_ARRANGEMENT_NIR)
+            isMonoChromeSupported =
+                (colorFilterArrangement == CameraMetadata.SENSOR_INFO_COLOR_FILTER_ARRANGEMENT_MONO
+                        || colorFilterArrangement == CameraMetadata.SENSOR_INFO_COLOR_FILTER_ARRANGEMENT_NIR)
         }
         return isPrivateImageFormatSupported && !isMonoChromeSupported
     }
